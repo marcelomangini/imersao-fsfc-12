bceafb3615984647db8faf92b7d42e5cab8e271b
Marcelo Mangini Guedes <marcelomangini@hotmail.com>
2023-02-07 19:17:47 -0300

    First Commit

---
+++ application/kafka/produce.go
@@ -0,0 +1,28 @@
+package kafka2
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/marcelomangini/imersaofsfc2-simulator/application/route"
+	"github.com/marcelomangini/imersaofsfc2-simulator/infra/kafka"
+)
+
+// {"clientId":"1","routeId":"1"}
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route.NewRoute()
+	json.Unmarshal(msg.Value, &route)
+	route.LoadPositions()
+	positions, err := route.ExportJsonPositions()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	for _, p := range positions {
+		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
